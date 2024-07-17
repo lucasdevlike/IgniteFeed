dade3b947d7563185b2aedabbe158a80d984543d
lucasfregonezi <lucasfregonezi@gmail.com>
2024-07-17 08:24:32 -0300

    Under development, creation of the layout with all components on screen and responsive.

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
+
+import './global.css';
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+
+          <Post author="Diego Fernandes" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+
+          <Post author="Diego Fernandes" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+
+        </main>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
